5396653b042932045b770899571e6abb71fc4bc8
sreeveena <sreeveena21@gmail.com>
2020-02-10 09:19:11 -0800

    fixed heroku db connection

--- config/connection.js
+++ config/connection.js
@@ -1,4 +1,5 @@
 var mongoose = require("mongoose");
 
-module.exports = mongoose.connect("mongodb://localhost/unit18Populater", { useNewUrlParser: true });
+// module.exports = mongoose.connect("mongodb://localhost/unit18Populater", { useNewUrlParser: true });
 
+module.exports = mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/unit18Populater", { useNewUrlParser: true });
